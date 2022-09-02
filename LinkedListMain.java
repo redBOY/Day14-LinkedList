e69b4c53163b1a4a99255e203ed91c0d2c132cc0
redBOY <43438363+redBOY@users.noreply.github.com>
2022-09-02 17:22:01 +0530

    uc5 commit

---
+++ LinkedListMain.java
@@ -0,0 +1,15 @@
+package com.bridgelabzLinkedList;
+
+public class LinkedListMain {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.push(70);
+        linkedList.push(56);
+
+        linkedList.insert(30);
+
+        System.out.println(linkedList.pop());
+
+        linkedList.display();
+    }
+}
